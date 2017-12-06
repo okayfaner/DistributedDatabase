f3abff30d10f0e351ab2ded2ae02c33cbc018ab1
范欧侃 <of394@nyu.edu>
2017-12-06 15:20:46 -0500

    "DistributedDatabase.java and TransactionManager.java"

--- src/edu/nyu/adb/DistributedDatabase.java
+++ src/edu/nyu/adb/DistributedDatabase.java
@@ -1,4 +1,8 @@
 package edu.nyu.adb;
 
 public class DistributedDatabase {
+    public static void main(String[] args) {
+        TransactionManager transactionManager = new TransactionManager(args[0]);
+        transactionManager.init();
+    }
 }
--- src/edu/nyu/adb/Site.java
+++ src/edu/nyu/adb/Site.java
@@ -5,40 +5,39 @@ import java.util.Date;
 
 public class Site {
 
-  private boolean siteStatus;
-  private int siteIndex;
-  private HashSet<Variable> variableSet;
+    private boolean siteStatus;
+    private int siteIndex;
+    private HashSet<Variable> variableSet;
 
-
-  public Site(int siteIndex) {
-    this.siteStatus = true;
-    this.siteIndex = siteIndex;
-    for (int i = 1; i <= 20; i++) {
-      if (i % 2 == 0 || siteIndex == 1 + i % 10) {
-        variableSet.add(new Variable(i, 10 * i));
-      }
+    public Site(int siteIndex) {
+        this.siteStatus = true;
+        this.siteIndex = siteIndex;
+        this.variableSet = new HashSet<>();
+        for (int i = 1; i <= 20; i++) {
+            if (i % 2 == 0 || siteIndex == 1 + i % 10) {
+                variableSet.add(new Variable(i, 10 * i));
+            }
+        }
     }
-  }
 
-  public boolean getSiteStatus() {
-    return this.siteStatus;
-  }
+    public boolean getSiteStatus() {
+        return this.siteStatus;
+    }
 
-  public void setSiteStatus(boolean siteStatus) {
-    this.siteStatus = siteStatus;
-  }
+    public void setSiteStatus(boolean siteStatus) {
+        this.siteStatus = siteStatus;
+    }
 
-  public int getSiteIndex() {
-    return this.siteIndex;
-  }
+    public int getSiteIndex() {
+        return this.siteIndex;
+    }
 
-  public void setSiteIndex(int siteIndex) {
-    this.siteIndex = siteIndex;
-  }
+    public void setSiteIndex(int siteIndex) {
+        this.siteIndex = siteIndex;
+    }
 
-  public void setVarValue(Variable variable, int newValue) {
-    variable.setValue(newValue);
-  }
-
+    public void setVarValue(Variable variable, int newValue) {
+        variable.setValue(newValue);
+    }
 
 }
